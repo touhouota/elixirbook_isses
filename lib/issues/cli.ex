ac6ed0fb1c2ac8690b210f20d0730c4e91565f47
touhouota <touhouota.register@gmail.com>
2019-06-01 23:31:22 +0900

    追加。

---
+++ lib/issues/cli.ex
@@ -0,0 +1,35 @@
+defmodule Issues.CLI do
+  @default 4
+
+  @moduledoc """
+  Handle the command line parsing and the dispatch to
+  the various functions that end up generating a
+  table of the last _n_ issues is a github project
+  """
+
+  def run do
+    parse_args(argv)
+  end
+
+  @doc """
+  `argv` can be -h  or --help, which returns :help.
+
+  Overwise it is a github user name, project name , and (optionally)
+  the number of entries to format.
+  Return a tuple of `{user, project, count}`, or `:help` if help was given.
+  """
+  def parse_args(argv) do
+    parse = OptionParser.parse(argv, switches: [ help: :boolean],
+                                     aliases:  [ h:    :help   ])
+
+    case parse do
+      {[help: true], _, _}
+        -> :help
+
+      {_, [user, project, count], _}
+        -> {user, project, @default_count}
+
+      _ -> :help
+    end
+  end
+end
