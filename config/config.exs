8cdc70d1c9059c40367e14593c68326a89fbf4d4
touhouota <touhouota.register@gmail.com>
2019-06-02 11:35:37 +0900

    modify:config.exs GithubのURLが直打ちだったのでconfigに吐き出し、そこから読み込むように変更。

--- config/config.exs
+++ config/config.exs
@@ -28,4 +28,4 @@ use Mix.Config
 # here (which is why it is important to import them last).
 #
 #     import_config "#{Mix.env()}.exs"
-config :isses, github_url: "https://api.github.com"
+config :issues, github_url: "https://api.github.com"
